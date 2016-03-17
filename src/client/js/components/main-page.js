21acb0e467f958a9a549d7d82c5434caeea48735
Ethan H. Jurman <ehj2229@rit.edu>
2016-03-17 15:25:28 -0400

    loads nice

--- src/client/js/components/main-page.js
+++ src/client/js/components/main-page.js
@@ -13,7 +13,7 @@ const paperStyle = {
 }
 
 const feeds = [
-  'http://espn.go.com/espn/rss/nhl/news'
+  'http://espn.go.com/espn/rss/news'
 ];
 
 export default class MainPage extends Component {
@@ -21,19 +21,25 @@ export default class MainPage extends Component {
     super(props);
     this.state = {
       feeds: [],
-      favorites: []
+      favorites: [],
+      maxFeeds: 0
     }
   }
 
   componentDidMount() {
     this.loadMultipleFeeds.bind(this)();
     setInterval(this.loadMultipleFeeds.bind(this), 10*1000);
+    window.addEventListener('scroll', this.handleScroll.bind(this));
 
+    for (let i=0; i < 10; i++){
+      setTimeout(()=>{
+        this.setState({maxFeeds: i})
+      }, i * 200);
+    }
   }
 
   loadMultipleFeeds(){
     feeds.forEach( (feed) => {
-      console.log("loading from " + feed);
       loadFeed(feed, this.addFeed.bind(this));
     });
   }
@@ -72,31 +78,41 @@ export default class MainPage extends Component {
     });
   }
 
+  handleScroll(event){
+    // buffer of 50px
+    if ((window.innerHeight + window.scrollY + 150) >= document.body.offsetHeight) {
+        // you're at the bottom of the page
+        this.setState({ maxFeeds: this.state.maxFeeds + 1});
+    }
+  }
+
   render() {
 
     if (this.state.feeds < 1) {
       return (<span> loading... </span>);
     }
 
-    const feedRender = this.state.feeds.map((entry) => {
-      return (
-        <Card className={'move-in'} style={paperStyle} key={entry.title}>
-          <CardHeader title={entry.title} subtitle={entry.pubDate}/>
-          <IconButton onClick={this.toggleFavorites.bind(this, entry)}>
-            { this.state.favorites.find((f)=>{
-              return f.title == entry.title
-            }) ? <FavoriteIcon color="red" /> : <FavoriteBorderIcon color="red"/>}
-          </IconButton>
-          <FlatButton
-            style={{float:'right'}}
-            label="Full Article"
-            labelPosition="before"
-            secondary={true}
-            linkButton={true}
-            href={entry.link}
-            icon={<ArrowRight/>} />
-        </Card>);
-    });
+    const feedRender = this.state.feeds
+      .filter((ele, i) => i < this.state.maxFeeds )
+      .map((entry) => {
+        return (
+          <Card className={'move-in'} style={paperStyle} key={entry.title}>
+            <CardHeader title={entry.title} subtitle={entry.pubDate}/>
+            <IconButton onClick={this.toggleFavorites.bind(this, entry)}>
+              { this.state.favorites.find((f)=>{
+                return f.title == entry.title
+              }) ? <FavoriteIcon color="red" /> : <FavoriteBorderIcon color="red"/>}
+            </IconButton>
+            <FlatButton
+              style={{float:'right'}}
+              label="Full Article"
+              labelPosition="before"
+              secondary={true}
+              linkButton={true}
+              href={entry.link}
+              icon={<ArrowRight/>} />
+          </Card>);
+      });
 
     return (
       <div style={{minWidth:'250px'}}>
